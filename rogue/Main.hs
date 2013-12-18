855cf5372de29e95351077735c91670b21506799
Trevor Elliott <trevor@galois.com>
2013-12-18 13:51:25 -0800

    Remove the SDL stuff

--- rogue/Main.hs
+++ rogue/Main.hs
@@ -1,34 +1,8 @@
 module Main where
 
-import qualified SDL
 import qualified Rogue
 
 import Control.Concurrent (threadDelay)
 
 main :: IO ()
 main  = Rogue.test
-
-{-
-main :: IO ()
-main  = SDL.withSDL SDL.initVideo $ do
-
-  win <- SDL.createWindow "rogue" (SDL.WindowPos 0 0) 640 480 SDL.windowOpenGL
-  cxt <- SDL.gl_createContext win
-
-  let loop = do evt <- SDL.pollEvent
-                case evt of
-                  SDL.Quit ->
-                    return ()
-
-                  SDL.KeyEvent ke ->
-                    print ke >> loop
-
-                  SDL.WindowEvent e ->
-                    print e >> loop
-
-                  _ -> threadDelay 1000 >> loop
-
-  loop
-
-  SDL.destroyWindow win
-  -}
